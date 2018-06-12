3b91c2382f44aaa5e5c40125b1d5a858cc10ed6b
juanmad1 <juanmad1@hotmail.com>
2018-06-12 15:24:14 +0200

    Nombre de usuario

--- php/funciones.php
+++ php/funciones.php
@@ -2,9 +2,10 @@
     function logueado() {
        session_start();
         if(isset($_SESSION['nick'])){
+            $nombre = explode('@',$_SESSION['nick']);
             echo '<script>$(document).ready(function (){
                 revelar();
-                let aux = "<p id=log>'.$_SESSION["nick"].'</p>";
+                let aux = "<p id=log>Hola, '.$nombre[0].'</p>";
                 $("#user_zona").append(aux);
             });</script>';
         } 
